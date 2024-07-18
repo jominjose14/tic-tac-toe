7ddcbaef733c29afb9089840d68bf143935d2c1f
unnamed-demon <ameermashruf@gmail.com>
2024-07-18 14:58:18 +0530

    make plays responsive and score board functional

--- script.js
+++ script.js
@@ -21,8 +21,16 @@ let print = () => {
         console.log(output);
     }
 }
+let clear = () => {
+    for(let i = 0; i < 3; i++)
+        for(let j = 0; j < 3; j++) 
+            grid[i][j] = ' ';
+    filled = 0;
+    slots.forEach((slot) => {
+        slot.textContent = '';
+    }); 
+}
 let checkWin = () => {
-    filled ++;
     if((grid[y][0] == grid[y][1] && grid[y][1] == grid[y][2]) || (grid[0][x] == grid[1][x] && grid[1][x] == grid[2][x])) 
         return true;
     if(x == y && grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2])
@@ -31,37 +39,50 @@ let checkWin = () => {
         return true;
 }
 
-let play = (row, col) => {
+let play = (row, col, event) => {
     if(grid[row][col] != ' ')
         return;
     x = col;
     y = row;
     if(p1turn) {
         cross();
+        event.target.textContent = "X";
+        filled ++;
         if(checkWin()) {
             score1++;
-            p1turn = true;
+            document.querySelector("#p1Score").textContent = score1;
+            p1turn = true;          
+            clear();
             // p1 win message
+            return;
         }
     }
     else {
         circle();
+        event.target.textContent = "O";
+        filled ++;
         if(checkWin()) {
             score2++;
+            document.querySelector("#p2Score").textContent = score2;
             p1turn = true;
+            clear();
             // p2 win message
+            return;
         }
     }
-    p1turn = !p1turn;
     if(filled == 9) {
         // draw message
+        clear();
+        p1turn = true;
+        return;
     }
+    p1turn = !p1turn;
 }
 
 const slots = document.querySelectorAll(".slot");
 slots.forEach((slot) => {
-    slot.addEventListener("click", () => {
-        play(Number(slot.getAttribute("data-row")), Number(slot.getAttribute("data-col")));
+    slot.addEventListener("click", (e) => {
+        play(Number(slot.getAttribute("data-row")), Number(slot.getAttribute("data-col")), e);
     });
 });
 
@@ -69,7 +90,7 @@ slots.forEach((slot) => {
 //     play();
 //     console.log(`Player 1's score is ${score[0]}.
 //                \nPlayer 2's score is ${score[1]}.`);
-//     for(let i = 0; i < 3; i++)
-//         for(let j = 0; j < 3; j++)
-//             grid[x][y] = ' ';
+    // for(let i = 0; i < 3; i++)
+    //     for(let j = 0; j < 3; j++)
+    //         grid[x][y] = ' ';
 // }
